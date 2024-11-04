d0926262f596932a01cb02967f01b4114f641a27
SimonS2019 <shentiexiong@gmail.com>
2024-11-05 12:10:02 +1300

    24. Data Annotations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
